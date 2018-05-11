df9d15e4cd527a2ec93f7f207819fdb6970fb266
davidnquach <davidnquach@gmail.com>
2018-05-10 22:15:24 -0400

    ASPSH-1012: Setup POC for pub/sub

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ import (
 	"net/http"
 )
 
-type Channel struct {
-	Name string `json:"name"`
-}
+// type Channel struct {
+// 	Name string `json:"name"`
+// }
 
-type Subscriber struct {
-	Name        string `json:"name"`
-	ChannelName string `json:"channelName"`
-}
+// type Subscriber struct {
+// 	Name        string `json:"name"`
+// 	ChannelName string `json:"channelName"`
+// }
 
 type ChannelMessage struct {
 	Channel string `json:"channel"`
@@ -28,85 +28,95 @@ var (
 	database = redis.DialDatabase(0)
 )
 
-func CreateChannel(w http.ResponseWriter, r *http.Request) {
-	conn, err := redis.Dial("tcp", ":6379", database)
-	defer conn.Close()
-	if err != nil {
-		fmt.Fprintf(w, "Server not available")
-		return
-	}
+// TODO Add channel to db
+// func CreateChannel(w http.ResponseWriter, r *http.Request) {
+// 	conn, err := redis.Dial("tcp", ":6379", database)
+// 	defer conn.Close()
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Server not available")
+// 		return
+// 	}
 
-	var channel Channel
+// 	var channel Channel
 
-	body, err := ioutil.ReadAll(r.Body)
-	err = json.Unmarshal(body, &channel)
-	if err != nil {
-		fmt.Fprintf(w, "unprocessable entity")
-		return
-	}
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	err = json.Unmarshal(body, &channel)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "unprocessable entity")
+// 		return
+// 	}
 
-	_, err = conn.Do("SET", channel.Name, 1)
-	if err != nil {
-		fmt.Fprintf(w, "channel not created")
-		return
-	}
+// 	_, err = conn.Do("SET", channel.Name, 1)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "channel not created")
+// 		return
+// 	}
 
-	fmt.Fprintf(w, "channel created!")
-}
+// 	fmt.Fprintf(w, "channel created!")
+// }
 
 func Publish(w http.ResponseWriter, r *http.Request) {
 	conn, err := redis.Dial("tcp", ":6379", database)
 	defer conn.Close()
 	if err != nil {
 		fmt.Fprintf(w, "Server not available")
+		return
 	}
 
-	var message ChannelMessage
+	var cMsg ChannelMessage
 
-	body, err := ioutil.ReadAll(r.Body)
-	err = json.Unmarshal(body, &message)
+	body, _ := ioutil.ReadAll(r.Body)
+	err = json.Unmarshal(body, &cMsg)
 	if err != nil {
 		fmt.Fprintf(w, "unprocessable entity")
+		return
 	}
 
-	available := conn.Do("GET", message.Channel)
-	if available == nil {
-		fmt.Fprintf(w, "channel not yet created")
-	} else {
-		conn.Do("SADD", message.Channel, message.Body)
-		fmt.Fprintf(w, "message published")
-	}
+	conn.Do("SADD", cMsg.Channel, cMsg.Body)
+	fmt.Fprintf(w, "message published")
 }
 
-func Subscribe(w http.ResponseWriter, r *http.Request) {
+// TODO add subscriber to db and add relationship to channel
+// func Subscribe(w http.ResponseWriter, r *http.Request) {
+// 	conn, err := redis.Dial("tcp", ":6379", database)
+// 	defer conn.Close()
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Server not available")
+// 		return
+// 	}
+
+// 	var subscriber Subscriber
+
+// 	body, _ := ioutil.ReadAll(r.Body)
+// 	err = json.Unmarshal(body, &subscriber)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "unprocessable entity")
+// 		return
+// 	}
+
+// 	available, _ := conn.Do("GET", subscriber.ChannelName)
+// 	if available == nil {
+// 		fmt.Fprintf(w, "channel not yet created")
+// 		return
+// 	}
+
+// 	key := fmt.Sprintf("%s:subscribers", subscriber.ChannelName)
+// 	conn.Do("SADD", key, subscriber.Name)
+
+// 	fmt.Fprintf(w, "subscribed")
+// }
+
+func GetMessage(w http.ResponseWriter, r *http.Request) {
 	conn, err := redis.Dial("tcp", ":6379", database)
 	defer conn.Close()
 	if err != nil {
 		fmt.Fprintf(w, "Server not available")
-		return
 	}
 
-	var subscriber Subscriber
+	vars := mux.Vars(r)
+	message, _ := redis.String(conn.Do("SPOP", vars["channel"]))
 
-	body, err := ioutil.ReadAll(r.Body)
-	err = json.Unmarshal(body, &subscriber)
-	if err != nil {
-		fmt.Fprintf(w, "unprocessable entity")
-	}
-
-	available := conn.Do("GET", subscriber.ChannelName)
-	if available == nil {
-		fmt.Fprintf(w, "channel not yet created")
-		return
-	}
-
-	key = Sprintf("%s:subscribers", subscriber.ChannelName)
-	conn.Do("SADD", key, subscriber.Name)
-
-	fmt.Fprintf(w, "subscribe")
-}
-
-func GetMessage(w http.ResponseWriter, _ *http.Request) {
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
 }
 
 func main() {
@@ -116,5 +126,6 @@ func main() {
 	router.HandleFunc("/subscribe", Subscribe).Methods("POST")
 	router.HandleFunc("/subscribe/{channel}/message", GetMessage).Methods("GET")
 
+	fmt.Println("Starting server on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
